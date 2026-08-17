b9f84d5aea11c60f0b7e6c5800281d5823face23
Alex Rutar <alex@rutar.org>
2026-08-17 10:29:07 +0200

    Update case fold table to Unicode 16

--- matcher/generate_case_fold_table.sh
+++ matcher/generate_case_fold_table.sh
@@ -2,12 +2,12 @@
 set -e
 
 dir=$(pwd)
-mkdir /tmp/ucd-15.0.0
-cd /tmp/ucd-15.0.0
-curl -LO https://www.unicode.org/Public/zipped/15.0.0/UCD.zip
+mkdir /tmp/ucd-16.0.0
+cd /tmp/ucd-16.0.0
+curl -LO https://www.unicode.org/Public/zipped/16.0.0/UCD.zip
 unzip UCD.zip
 
 cd "${dir}"
 cargo install ucd-generate
-ucd-generate case-folding-simple /tmp/ucd-15.0.0 --chars > src/chars/case_fold.rs
-rm -rf /tmp/ucd-15.0.0
+ucd-generate case-folding-simple /tmp/ucd-16.0.0 --chars > src/chars/case_fold.rs
+rm -rf /tmp/ucd-16.0.0
